cfd3a61679a70dec9abe41d55eb75fbb59af49d4
shiragaki <sirashou55@yahoo.co.jp>
2023-09-09 23:19:50 +0900

    VNVB-40/searchDesiredLevel

--- frontend/src/components/ScheduleSearchDesireLevel/ScheduleSearchDesireLevel.tsx
+++ frontend/src/components/ScheduleSearchDesireLevel/ScheduleSearchDesireLevel.tsx
@@ -1,10 +1,16 @@
 import { Box, Rating } from "@mui/material";
 import { useState } from "react";
-// import StarIcon from "@mui/icons-material/Star";
+import { ScheduleCondition } from "../../pages/scheduleList";
 
-export const ScheduleSearchDesireLevel = () => {
+type Props = {
+  scheduleCondition: ScheduleCondition;
+  setScheduleCondition: (scheduleCondition: ScheduleCondition) => void;
+};
+export const ScheduleSearchDesireLevel = ({
+  scheduleCondition,
+  setScheduleCondition,
+}: Props) => {
   const [desireLevel, setDesireLevel] = useState<number>(0);
-  // const [hover, o0lsetHover] = useState(-1);
   return (
     <Box display={"flex"} sx={{ marginRight: "20px" }}>
       <p>希望度：</p>
@@ -14,14 +20,16 @@ export const ScheduleSearchDesireLevel = () => {
         data-testid="form-rating"
         sx={{ paddingTop: "15px" }}
         onChange={(_, newValue) => {
-          typeof newValue === "number"
-            ? setDesireLevel(newValue)
-            : setDesireLevel(0);
+          if (typeof newValue === "number") {
+            setScheduleCondition({
+              ...scheduleCondition,
+              desiredLevel: newValue,
+            });
+            setDesireLevel(newValue);
+          } else {
+            setDesireLevel(0);
+          }
         }}
-        // onChangeActive={(event, newHover) => {
-        //   setHover(newHover);
-        // }}
-        // emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
       />
     </Box>
   );
